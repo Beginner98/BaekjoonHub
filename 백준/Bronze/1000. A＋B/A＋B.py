0bced1e3ecceecc90d3ac97f73b26ee1ee6fdf01
Uijin <108450394+Beginner98@users.noreply.github.com>
2022-07-20 01:06:04 +0900

    Update A＋B.py

--- 백준/Bronze/1000. A＋B/A＋B.py
+++ 백준/Bronze/1000. A＋B/A＋B.py
@@ -1,2 +1,12 @@
+# map 함수 사용 방법
+# https://dojang.io/mod/page/view.php?id=2179
+
+# m1
+#a, b = input().split()
+#a = int(a)
+#b = int(b)
+#print(a+b)
+
+#m2
 a, b = map(int, input().split())
 print(a + b)
